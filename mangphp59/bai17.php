37260fc37b45426c909896addbb2f7e114f718e9
hungleutt <hungle230398@gmail.com>
2019-08-22 20:28:04 +0700

    mangphp59-2

---
+++ mangphp59/bai17.php
@@ -0,0 +1,7 @@
+<?php
+function min_values_not_zero(Array $values) 
+{
+return min(array_diff(array_map('intval', $values), array(0)));
+}
+print_r(min_values_not_zero(array(-1,0,1,12,-100,1))."\n");
+?>
